4e6bdf1ea1dc90b1e3dd3b9f9976ec96a7600cca
hosseinbahramigit <bahramihossein279@20minutemail.it>
2019-05-15 15:38:21 +0430

    Repository Controller

---
+++ WebProject/Controllers/repositoryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebPtoject.ExtraFunction;
+
+namespace WebProject.Controllers
+{
+    public class repositoryController : Controller
+    {
+        // GET: repository
+        public ActionResult Index()
+        {             
+            return View();
+        }
+
+        public ActionResult SelectData(int? id)
+        {
+            var T= Models.repositoryS.SelectData(id);
+            return View(T);
+        }
+
+        public ActionResult InsertData(string name, string address, string employee, string total_repository_count, string description)
+        {
+            var T = Models.repositoryS.InsertData(name, address, employee, total_repository_count, description);
+            return View(T);
+        }
+
+
+        public ActionResult UpdateData(string id, string name, string address, string employee, string total_repository_count, string description)
+        {
+            var T = Models.repositoryS.UpdateData(id, name, address, employee, total_repository_count, description);
+            return View(T);
+        }
+
+
+        public ActionResult DeleteData(string id)
+        {
+            var T = Models.repositoryS.DeleteData(id);
+            return View(T);
+        }
+    }
+}
